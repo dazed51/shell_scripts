52c6bed2d40d5cf6b6dc0439544bc19ce4ec0f0c
djb <dave.battaglia@gmail.com>
2018-04-10 07:00:07 -0400

    script to be used in conjunction with ansible playbook

---
+++ sys_deploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#install git
+
+#grab repo
+
+echo -e  "script to install git and other pre-req packages.."
+
+sleep 2
+
+echo -e "this script will also install ansible, and clone it"
+
+gitinstall=$(apt-get install git -y )
+ansiblerepo=$(apt-get install software-properties-common -y) 
+pull=$(apt-add-repository ppa:ansible/ansible -y)
+updaterepo=$(apt-get update && apt-get upgrade -y)
+ansibleinstall=$(apt-get install ansible -y)
+getatom=$(wget https://atom.io/download/deb)
+
+echo -e "installing git" $gitinstall
+echo -e "installing commons" $ansiblerepo
+echo -e " installing ansible repo" $pull
+
+sleep 2
+
+echo "installing ansible" $ansibleinstall
+echo "updating system" $upaterepo
+
+echo -e "\n\n"
+
+if [ $? -eq 0 ]; then
+    echo -e "git installed successfully..verifying" 
+    which git
+    which ansible
+fi
+
+sleep 3
+
+
+sleep 2
+dpkg -i *.deb
+echo -e "grabbing atom text editor and installing" $getatom
+
+echo -e "cloning repo sys setup repo"
+
+sleep 5
+
+git clone https://github.com/dazed51/ansible.git
+
+echo -e "\n\n"
+
+echo "pulling ansible system config"
+ansible-pull -U https://github.com/dazed51/ansible.git
